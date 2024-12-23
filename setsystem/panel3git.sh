bd32735db282f3b78bfd5123b41d521cdb277da1
沐潇MXine <muxiao365@88.com>
2024-12-23 04:34:34 +0000

    Update panel3git.sh

--- setsystem/panel3git.sh
+++ setsystem/panel3git.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
 # ------------------------------Heak------------------------------
+#!/bin/bash
 # 获取当前脚本的目录
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LOG_DIR="$SCRIPT_DIR/log"
@@ -14,27 +14,38 @@ touch "$LOG_COST"
 # ------------------------------Mo[TWO]ok------------------------------
 # Ubuntu&Debian-seew
 debianseew() {
-    install_dependencies() || { echo "install_dependencies 失败" | tee -a "$LOG_FILE"; return 1; }
-}
-
-install_dependencies() {
-    packages=("jq" "curl" "git" "wget" "sed" "awk" "grep")
-    for package in "${packages[@]}"; do
-        if ! command -v "$package" &> /dev/null; then
-            echo "$package 未安装，正在执行安装脚本..." | tee -a "$LOG_FILE"
-            sudo apt-get install -y "$package"
-            if [ $? -ne 0 ]; then
-                echo "$package 脚本执行失败" | tee -a "$LOG_FILE"
-                return 1
+    install_dependencies() {
+        packages=("jq" "curl" "git" "wget" "sed" "awk" "grep")
+        for package in "${packages[@]}"; do
+            if ! command -v "$package" &> /dev/null; then
+                echo "$package 未安装，正在执行安装脚本..." | tee -a "$LOG_FILE"
+                (sudo apt-get update && sudo apt-get install -y "$package") 2>&1 | tee -a "$LOG_FILE"
+                if [ $? -ne 0 ]; then
+                    echo "$package 脚本执行失败" | tee -a "$LOG_FILE"
+                    return 1
+                fi
             fi
-        fi
-    done
+        done
+    } || { echo "install_dependencies 失败" | tee -a "$LOG_FILE"; return 1; }
 }
 
 # CentOS&RedHat-seew
 redhatseew() {
-    install_dependencies || { echo "install_dependencies 失败" | tee -a "$LOG_FILE"; return 1; }
+    install_dependencies() {
+        packages=("jq" "curl" "git" "wget" "sed" "awk" "grep")
+        for package in "${packages[@]}"; do
+            if ! command -v "$package" &> /dev/null; then
+                echo "$package 未安装，正在执行安装脚本..." | tee -a "$LOG_FILE"
+                (sudo yum update -y && sudo yum install -y "$package") 2>&1 | tee -a "$LOG_FILE"
+                if [ $? -ne 0 ]; then
+                    echo "$package 脚本执行失败" | tee -a "$LOG_FILE"
+                    return 1
+                fi
+            fi
+        done
+    } || { echo "install_dependencies 失败" | tee -a "$LOG_FILE"; return 1; }
 }
+
 
 # Ubuntu&Debian-1Panel
 debian-panel() {
@@ -60,41 +71,41 @@ redhat-panel() {
 
 # Docker加速函数
 fastdocker() {
-    echo "1Panel已安装，正在执行加速脚本..." | tee -a "$LOG_FILE"
-    mkdir -p /etc/docker
-    touch daemon.json
-    touch daemon.conf
-    sudo cp /etc/docker/daemon.json /etc/djson.ini
-    sudo cp /etc/docker/daemon.conf /etc/dconf.ini
-    tee /etc/docker/daemon.json <<-'EOF'
-    {
-        "registry-mirrors": [
-            "https://docker.1panel.live"
-        ]
-    }
-EOF
-    systemctl daemon-reload | tee -a "$LOG_FILE"
-    systemctl stop docker.service | tee -a "$LOG_FILE"
-    systemctl stop docker.socket | tee -a "$LOG_FILE"
-    systemctl start docker.service | tee -a "$LOG_FILE"
-    systemctl start docker.socket | tee -a "$LOG_FILE"
-    systemctl restart docker | tee -a "$LOG_FILE"
-    if [ $? -ne 0 ]; then
-        echo "Docker配置脚本执行失败" | tee -a "$LOG_FILE"
-        cd /etc/docker/ 
-        mv daemon.conf daemon.json -y
-        sudo cp /etc/djson.ini /etc/docker/daemon.json
-        sudo cp /etc/dconf.ini /etc/docker/daemon.conf
-        systemctl stop docker.service | tee -a "$LOG_FILE"
-        systemctl stop docker.socket | tee -a "$LOG_FILE"
-        systemctl start docker.service | tee -a "$LOG_FILE"
-        systemctl start docker.socket | tee -a "$LOG_FILE"
-        systemctl restart docker | tee -a "$LOG_FILE"
-        return 1
-    fi
+	echo "1Panel已安装，正在执行加速脚本..." | tee -a "$LOG_FILE"
+	mkdir -p /etc/docker
+	touch daemon.json
+	touch daemon.conf
+	cp /etc/docker/daemon.json /etc/djson.ini -y
+	cp /etc/docker/daemon.conf /etc/dconf.ini -y
+	tee /etc/docker/daemon.json <<-'EOF'
+	{
+		"registry-mirrors": [
+			"https://docker.1panel.live"
+		]
+	}
+	EOF
+	systemctl daemon-reload | tee -a "$LOG_FILE"
+	systemctl stop docker.service | tee -a "$LOG_FILE"
+	systemctl stop docker.socket | tee -a "$LOG_FILE"
+	systemctl start docker.service | tee -a "$LOG_FILE"
+	systemctl start docker.socket | tee -a "$LOG_FILE"
+	systemctl restart docker | tee -a "$LOG_FILE"
+	if [ $? -ne 0 ]; then
+		echo "Docker配置脚本执行失败" | tee -a "$LOG_FILE"
+		cd /etc/docker/ 
+		mv daemon.json daemon.conf -y
+		cp /etc/djson.ini /etc/docker/daemon.json -y
+		cp /etc/dconf.ini /etc/docker/daemon.conf -y
+		systemctl stop docker.service | tee -a "$LOG_FILE"
+		systemctl stop docker.socket | tee -a "$LOG_FILE"
+		systemctl start docker.service | tee -a "$LOG_FILE"
+		systemctl start docker.socket | tee -a "$LOG_FILE"
+		systemctl restart docker | tee -a "$LOG_FILE"
+		return 1
+	fi
 }
 
-# 1Panel三方app加载函数
+# 1panel三方加载函数
 panel3apps() {
     echo "1Panel已安装，正在执行更新脚本..." | tee -a "$LOG_FILE"
     git clone -b localApps https://github.com/okxlin/appstore /opt/1panel/resource/apps/local/appstore-localApps
@@ -107,38 +118,37 @@ panel3apps() {
         echo "Panel3Git复制文件失败" | tee -a "$LOG_FILE"
         exit 1
     fi
-    rm -rf /opt/1panel/resource/apps/local/appstore-localApps
+    rm -r /opt/1panel/resource/apps/local/appstore-localApps
     if [ $? -ne 0 ]; then
         echo "Panel3Git删除缓存失败" | tee -a "$LOG_FILE"
         exit 1
     fi
 }
-
 # ------------------------------Mo[ONE]ok------------------------------
 # 定义DebianMook函数
 Debianmook() {
-    echo "Running Debianmook function" | tee -a "$LOG_FILE"
-    if command -v 1panel &> /dev/null; then
-        panel3apps
-    else
-        debianseew
-        debian-panel
-        fastdocker
-        panel3apps
-    fi
+	echo "Running Debianmook function" | tee -a "$LOG_FILE"
+	if command -v 1panel &> /dev/null; then
+		panel3apps
+	else
+		debianseew
+		debian-panel
+		fastdocker
+		panel3apps
+	fi
 }
 
 # 定义RedhatMook函数
 Redhatmook() {
     echo "Running Redhatmook function" | tee -a "$LOG_FILE"
-    if command -v 1panel &> /dev/null; then
-        panel3apps
-    else
-        redhatseew
-        redhat-panel
-        fastdocker
-        panel3apps
-    fi
+	if command -v 1panel &> /dev/null; then
+		panel3apps
+	else
+		redhatseew
+		redhat-panel
+		fastdocker
+		panel3apps
+	fi
 }
 # ------------------------------ReskONE------------------------------
 ReskONE() {
@@ -173,29 +183,32 @@ ReskONE() {
             log_and_run() {
                 local log_value=$1
                 local function_name=$2
-                echo $log_value > "$LOG_COST"
+
+                echo "$log_value" > "$LOG_COST"
                 $function_name
             }
 
             case $OS in
                 ubuntu|debian)
-                    log_and_run T Debianmook
+                    log_and_run "T"
                     ;;
                 centos|redhat)
-                    log_and_run F Redhatmook
+                    log_and_run "F"
                     ;;
                 *)
                     echo "Unsupported OS: $OS"
-                    echo "Please select the OS type: Debian Series (T) or RedHat Series (F)"
+                    echo "Please select the OS type:"
+                    echo "Debian series (T) or RedHat series (F)"
                     while true; do
                         read -p "Enter your choice (T/F): " CHOICE
+                        
                         case $CHOICE in
                             T)
-                                log_and_run T Debianmook
+                                log_and_run "T"
                                 break
                                 ;;
                             F)
-                                log_and_run F Redhatmook
+                                log_and_run "F"
                                 break
                                 ;;
                             *)
@@ -209,4 +222,5 @@ ReskONE() {
     done
 }
 # ------------------------------THEEND------------------------------
+# 初始MOOK
 ReskONE
